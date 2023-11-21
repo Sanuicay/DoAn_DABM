95a0cab0f3508b719e0c0987d8008451fbc4547f
boochou <chaunguyen27423@gmail.com>
2023-11-21 21:47:36 +0700

    test

---
+++ database_scripts/connect.php
@@ -0,0 +1,13 @@
+<?php
+$mysqli = new mysqli("localhost:3306","root","","doantest");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+$mysqli -> set_charset("utf8");
+
+
+
+?>
